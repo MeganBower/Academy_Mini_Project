9c1b078c854089ac87a5d90df0b913893d038492
MeganBower <megan.bower@infinityworks.com>
2021-10-08 10:47:07 +0100

    got rid of extra space round table display, and semi colons

--- Connect_Four/connectors.js
+++ Connect_Four/connectors.js
@@ -2,11 +2,11 @@
 // You should not have to make any changes in this file to get your game working.
 
 // Validate academite functions are available
-const functions = ["takeTurn", "getBoard", "checkWinner", "resetGame"];
+const functions = ["takeTurn", "getBoard", "checkWinner", "resetGame"]
 for (f of functions) {
-    const functionObject = window[f];
+    const functionObject = window[f]
     if (typeof functionObject !== "function") {
-        throw `Looks like expected function '${f}' is missing. Double check the function signatures from academy.js are still present and unaltered.`;
+        throw `Looks like expected function '${f}' is missing. Double check the function signatures from academy.js are still present and unaltered.`
     }
 }
 
@@ -27,39 +27,37 @@ function drawBoard(board) {
             if (!board[rowIndex][columnIndex]) {
                 continue;
             }
-            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = board[rowIndex][columnIndex] === "purple" ? "🟣" : "🟠";
+            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = board[rowIndex][columnIndex] === "purple" ? "🟣" : "🟠"
         }
     }
 }
 
 function isValidRowOrColumn(array) {
-    return Array.isArray(array) && array.length === 6 || array.length === 7;
+    return Array.isArray(array) && array.length === 6 || array.length === 7
 }
 
 function isValidColumn(columnArray) {
-    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["purple", "orange", null].includes(item); });
+    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["purple", "orange", null].includes(item); })
 }
 
 // A grid position was clicked call the game's turn function, redraw and then check for a winner.
 function positionClick(rowIndex, columnIndex, event) {
-    // const player1Display = document.getElementById("first-player-display")
-    // player1Display.style.display = "none"
 
-    const errorColumnFull = checkingNextEmptyRow(columnIndex);
+    const errorColumnFull = checkingNextEmptyRow(columnIndex)
     if (errorColumnFull === 'full') {
-        const errorDisplay = document.getElementById("column-full-display");
-        errorDisplay.style.display = "block";
+        const errorDisplay = document.getElementById("column-full-display")
+        errorDisplay.style.display = "block"
         function closeError() {
-            document.getElementById("column-full-display").style.display = " none";
+            document.getElementById("column-full-display").style.display = " none"
         }
         window.setTimeout(closeError, 2000)
 
     }
 
-    takeTurn(rowIndex, columnIndex);
-    const board = getBoard();
+    takeTurn(rowIndex, columnIndex)
+    const board = getBoard()
     if (!isValidRowOrColumn(board) || !board.every(isValidColumn)) {
-        throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board);
+        throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board)
     }
     
     const colour = playerTurnColour()
@@ -69,32 +67,32 @@ function positionClick(rowIndex, columnIndex, event) {
     
     const playerName = document.getElementById("player-name")
     playerName.innerText = player + ' (' + colour + ')'
-    const playerDisplay = document.getElementById("player-turn-display");
+    const playerDisplay = document.getElementById("player-turn-display")
     playerDisplay.style.display = "block"
 
-    drawBoard(board);
-    const winner = checkWinner();
+    drawBoard(board)
+    const winner = checkWinner()
     if (winner) {
         if (typeof winner !== "string" || !["purple", "orange", "nobody"].includes(winner)) {
-            throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner;
+            throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner
         }
         document.getElementById("player1-wins").innerText = storePlayer1Wins()
         document.getElementById("player2-wins").innerText = storePlayer2Wins()
-        const winnerName = document.getElementById("winner-name");
+        const winnerName = document.getElementById("winner-name")
         const playerTurn = playerTurnName()
         const player1Name = document.getElementById("player1-Name").value
         const player2Name = document.getElementById("player2-Name").value
         const winningPlayer = (playerTurn === 'player1') ? player2Name : player1Name
-        winnerName.innerText = `${winningPlayer} (${winner})`; // turnary operator to change player 1/2
-        const winnerDisplay = document.getElementById("winner-display");
-        winnerDisplay.style.display = "block";
+        winnerName.innerText = `${winningPlayer} (${winner})`
+        const winnerDisplay = document.getElementById("winner-display")
+        winnerDisplay.style.display = "block"
         playerDisplay.style.display = "none"
     }
 }
 
 // The reset button was clicked, call the game's reset function then reset the DOM.
 function resetClick(event) {
-    resetGame();
+    resetGame()
     // const player = playerTurnName()
     const colour = playerTurnColour()
     const player1Name = document.getElementById("player1-Name").value
@@ -102,30 +100,28 @@ function resetClick(event) {
     const player = (colour === 'purple') ? player1Name : player2Name
     const playerName = document.getElementById("player-name")
     playerName.innerText = player + ' (' + colour + ')'
-    const playerDisplay = document.getElementById("player-turn-display");
+    const playerDisplay = document.getElementById("player-turn-display")
     playerDisplay.style.display = "block"
 
-    const winnerName = document.getElementById("winner-name");
-    winnerName.innerText = "";
-    const winnerDisplay = document.getElementById("winner-display");
-    winnerDisplay.style.display = "None";
+    const winnerName = document.getElementById("winner-name")
+    winnerName.innerText = ""
+    const winnerDisplay = document.getElementById("winner-display")
+    winnerDisplay.style.display = "None"
 
-    // const player1Display = document.getElementById("first-player-display")
-    // player1Display.style.display = "block"
-    clearBoard();
+    clearBoard()
 }
 
 // Bind the click events for the grid.
 for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
     for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
-        const gridPosition = document.getElementById(`row-${rowIndex}-column-${columnIndex}`);
-        gridPosition.addEventListener("click", positionClick.bind(null, rowIndex, columnIndex));
+        const gridPosition = document.getElementById(`row-${rowIndex}-column-${columnIndex}`)
+        gridPosition.addEventListener("click", positionClick.bind(null, rowIndex, columnIndex))
     }
 }
 
 // Bind the click event for the reset button.
-const resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", resetClick);
+const resetButton = document.getElementById("reset-button")
+resetButton.addEventListener("click", resetClick)
 
 window.onload = function () {
     const colour = playerTurnColour()
@@ -134,7 +130,7 @@ window.onload = function () {
     const player = (colour === 'purple') ? player1Name : player2Name
     const playerName = document.getElementById("player-name")
     playerName.innerText = player + ' (' + colour + ')'
-    const playerDisplay = document.getElementById("player-turn-display");
+    const playerDisplay = document.getElementById("player-turn-display")
     playerDisplay.style.display = "block"
     document.getElementById("player1-wins").innerText = '0'
     document.getElementById("player2-wins").innerText = '0'
